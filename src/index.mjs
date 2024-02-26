d3fba56ef2bb1c2d0e02a9a52dab281210c31d4c
Samuel581 <00023020@uca.edu.sv>
2024-02-26 17:48:50 -0600

    :construction: Add test cookie
    Added a test cookie to play around with it's behavior and usage

--- src/index.mjs
+++ src/index.mjs
@@ -2,26 +2,30 @@ import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes/index.mjs'
 import { loggingMiddleware } from './utils/middlewares.mjs';
+import cookieParser from 'cookie-parser';
 
 dotenv.config({ path: '.env' });
 const app = express();
 //Middleware
 app.use(express.json());
-app.use('/api',routes);
+app.use(cookieParser())
+app.use('/api', routes);
+
 
 const PORT = process.env.PORT;
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
 
 app.get('/', loggingMiddleware, (request, response, next) => {
     console.log('Base URL');
     next();
 }, (request, response) => {
+    response.cookie('hello', 'world', { maxAge: 60000 * 60 });
     response.status(201).send({ msg: 'Hello World' });
 })
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
 
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,8 +3,11 @@ import { mockProducts } from "../utils/constants.mjs";
 const router = Router();
 
 router.get('/products', (request, response) => {
-    console.log(request.query);
-    response.send(mockProducts)
+    console.log(request.headers.cookie);;
+    console.log(request.cookies);
+    if(request.cookies.hello && request.cookies.hello === 'world')
+        return response.send([{id:123, name: 'chicken', price: 5.75}]);
+    return response.send({msg: 'Sorry, you need the correct cookie'});
 });
 
 //Using params
